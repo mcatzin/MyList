b1387d32ca84c904f41f0654eb95cb32ad84bf58
Mario Catzin <catzin21@gmail.com>
2020-07-08 10:36:51 -0700

    filter function

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
+
 
 function addTodo(event) {
     event.preventDefault();
@@ -40,6 +43,7 @@ function deleteCheck(event) {
     console.log(event);
     if (item.classList[0] == "trash-btn") {
         const todo = item.parentElement;
+        //Animation
         todo.classList.add("fall");
         todo.addEventListener("transitionend", () => {
             todo.remove();
@@ -48,6 +52,31 @@ function deleteCheck(event) {
 
     if (item.classList[0] == "complete-btn") {
         const todo = item.parentElement;
-        todo.classList.toggle("complete")
+        todo.classList.toggle("completed")
     }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach((todo) => {
+        switch (event.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    })
 }
